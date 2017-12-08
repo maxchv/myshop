92bf7ee15679d81342a3f8e243cdeb942bb24140
Maxim <maxshaptala@gmail.com>
2017-12-08 19:06:27 +0200

    add url to robots and favicon

--- myshop/urls.py
+++ myshop/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^', include('shop.urls', namespace='shop')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico'), name='favicon'),
+    url(r'^robots\.txt$', RedirectView.as_view(url='/static/robots.txt'), name='robots'),
 ]
 
 if settings.DEBUG:
